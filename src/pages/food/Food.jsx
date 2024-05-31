be360095537808411b11ede8ff55570f8cad5335
harunnn035 <harundag0101@gmail.com>
2024-05-31 16:02:59 +0300

    update

--- src/pages/food/Food.jsx
+++ src/pages/food/Food.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import TabMenu from '../../components/tabMenu/TabMenu'
-export default function Food() {
-  return (
-    <div className='food'>
-      <TabMenu/>
-   <div className="main-food">Ana Yemek</div>
-   <div className="">Ara yemek</div>
-   <div className="drinks">İçecek</div>
-   <div className="dessert">Tatlı</div>
-    </div>
-  )
-}
+  import React, { useState } from 'react'
+  import TabMenu from '../../components/tabMenu/TabMenu'
+  import Foods from '../../components/food/Foods';
+
+
+  export default function Food() {
+    const [selectButton, setSelectButton] = useState("1");
+
+    return (
+      <div className='food'>
+        <TabMenu setSelectButton={setSelectButton} selectButton={selectButton} />
+        <div className='menus'>
+          <div style={selectButton === '1' ? { display: "block" } : { display: "none" }} className='main-food'>
+            <Foods />
+          </div>
+
+          <div style={selectButton === '2' ? { display: "block" } : { display: "none" }} className='snack'>Ara Yemek</div>
+          <div style={selectButton === '3' ? { display: "block" } : { display: "none" }} className='drinks'>İçecek</div>
+          <div style={selectButton === '4' ? { display: "block" } : { display: "none" }} className='dessert'>Tatlı</div>
+
+        </div>
+      
+      </div>
+      
+
+    )
+  }
