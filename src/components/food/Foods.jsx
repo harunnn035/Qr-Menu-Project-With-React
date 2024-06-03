5c18ffb6b5dd86d914a65d6ef310d8802948b5d0
harunnn035 <harundag0101@gmail.com>
2024-06-03 11:06:22 +0300

    Update Foods.jsx

--- src/components/food/Foods.jsx
+++ src/components/food/Foods.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
-function Food() {
+export default function Foods() {
   return (
-    <div>
-      
+    <div className="snack-container">
+      <div className="snack-container__item">
+      <figure><img src="" alt="yemek" /></figure>
+      <div className="snack-container__item__contect">
+      <div className="snack-container__item__content__header">Karışık Izgara</div>
+      <div className="snack-container__item__content__food-info ">
+<div className="snack-container__item__content__food-info__header" >İçindekiler</div>
+<ul>
+<li>Adana Kebap</li>
+<li>Tavuk Izgara</li>
+<li>Lahmacun</li>
+<li>Kuşbaşı</li>
+  
+</ul>
+      </div>
+      </div>
+      </div>
     </div>
   )
 }
-
-export default Food
