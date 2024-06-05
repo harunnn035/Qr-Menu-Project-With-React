5c7cd842ad9c733e0c2c23495bb8accd1da0d28e
harunnn035 <harundag0101@gmail.com>
2024-06-05 12:02:34 +0300

    update

---
+++ src/components/order/FoodList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function FoodList() {
+  return (
+  <div className='food-list'>
+<figure className='food-list__img'>
+    <img src="" alt="" />
+</figure>
+<div className='food-list__header'>
+<span className='food-list__header__title' >Karışık Izgara</span> 
+<span className='food-list__header__count' >1 Adet</span>
+</div>
+<div className='food-list__price' >180₺</div>
+
+  </div>
+  )
+}
--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-
+import FoodList from "../../components/order/FoodList";
+import Price from "../../components/order/Price";
 export default function Order() {
   return (
-    <div>
-      Order
-      
+    <div className="order">
+        <FoodList />
+        <Price />
     </div>
   )
 }
