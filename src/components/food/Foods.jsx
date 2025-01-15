ca4183936b35e32553d94d84a477a80cb871755a
harunnn035 <harundag0101@gmail.com>
2025-01-15 16:28:06 +0300

    up

--- src/components/food/Foods.jsx
+++ src/components/food/Foods.jsx
@@ -1,59 +1,41 @@
+import React from 'react';
+
+const yemekler = [
+  {
+    id: 1,
+    fiyat: '180 ₺',
+    isim: 'Karışık Izgara',
+    resim: 'assets/image/ana_yemek.svg',
+    icindekiler: ['Adana Kebap', 'Tavuk Izgara', 'Lahmacun', 'Kuşbaşı']
+  },
+  {
+    id: 2,
+    fiyat: '180 ₺',
+    isim: 'Karışık Izgara',
+    resim: 'assets/image/ana_yemek.svg',
+    icindekiler: ['Adana Kebap', 'Tavuk Izgara', 'Lahmacun', 'Kuşbaşı']
+  },
+  {
+    id: 3,
+    fiyat: '180 ₺',
+    isim: 'Karışık Izgara',
+    resim: 'assets/image/ana_yemek.svg',
+    icindekiler: ['Adana Kebap', 'Tavuk Izgara', 'Lahmacun', 'Kuşbaşı']
+  },
+  
+
+  
+];
+
 export default function Foods() {
   return (
-      <div className="snack-container">
-          <div className="snack-container__item">
-              <span className="snack-container__item__price">180 ₺</span>
-              <figure><img src="assets/image/ana_yemek.svg" alt="yemek" /></figure>
-              <div className="snack-container__item__content">
-                  <div className="snack-container__item__content__header">Karışık Izgara</div>
-                  <div className="snack-container__item__content__food-info">
-                      <div className="snack-container__item__content__food-info__header">İçindekiler</div>
-                      <ul>
-                          <li>Adana Kebap</li>
-                          <li>Tavuk Izgara</li>
-                          <li>Lahmacun</li>
-                          <li>Kuşbaşı</li>
-                      </ul>
-                  </div>
-              </div>
-              <div className="snack-container__item__add-card"><button>Sepete Ekle</button></div>
-          </div>
-
-          <div className="snack-container__item">
-              <span className="snack-container__item__price">180 ₺</span>
-              <figure><img src="assets/image/ana_yemek.svg" alt="yemek" /></figure>
-              <div className="snack-container__item__content">
-                  <div className="snack-container__item__content__header">Karışık Izgara</div>
-                  <div className="snack-container__item__content__food-info">
-                      <div className="snack-container__item__content__food-info__header">İçindekiler</div>
-                      <ul>
-                          <li>Adana Kebap</li>
-                          <li>Tavuk Izgara</li>
-                          <li>Lahmacun</li>
-                          <li>Kuşbaşı</li>
-                      </ul>
-                  </div>
-              </div>
-              <div className="snack-container__item__add-card"><button>Sepete Ekle</button></div>
-          </div>
-
-          <div className="snack-container__item">
-              <span className="snack-container__item__price">180 ₺</span>
-              <figure><img src="assets/image/ana_yemek.svg" alt="yemek" /></figure>
-              <div className="snack-container__item__content">
-                  <div className="snack-container__item__content__header">Karışık Izgara</div>
-                  <div className="snack-container__item__content__food-info">
-                      <div className="snack-container__item__content__food-info__header">İçindekiler</div>
-                      <ul>
-                          <li>Adana Kebap</li>
-                          <li>Tavuk Izgara</li>
-                          <li>Lahmacun</li>
-                          <li>Kuşbaşı</li>
-                      </ul>
-                  </div>
-              </div>
-              <div className="snack-container__item__add-card"><button>Sepete Ekle</button></div>
-          </div>
-      </div>
-  )
+    <div className="snack-container">
+      {yemekler.map((yemek) => (
+        <div className="foods-container">
+          <div className="food-left">{yemek.isim}</div>
+          <div className="food-right">{yemek.fiyat}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/components/order/FoodList.jsx
+++ src/components/order/FoodList.jsx
@@ -1,17 +1,57 @@
-import React from 'react'
+import React from 'react';
+
+const yemekler = [
+  {
+    id: 1,
+    isim: 'Karışık Izgara',
+    adet: '1 Adet',
+    fiyat: '180 ₺',
+    resim: 'assets/image/ana_yemek.svg'
+  },
+  {
+    id: 2,
+    isim: 'Karışık Izgara',
+    adet: '1 Adet',
+    fiyat: '180 ₺',
+    resim: 'assets/image/ana_yemek.svg'
+  },
+  {
+    id: 3,
+    isim: 'Karışık Izgara',
+    adet: '1 Adet',
+    fiyat: '180 ₺',
+    resim: 'assets/image/ana_yemek.svg'
+  },
+  {
+    id: 4,
+    isim: 'Karışık Izgara',
+    adet: '1 Adet',
+    fiyat: '180 ₺',
+    resim: 'assets/image/ana_yemek.svg'
+  }
+];
 
 export default function FoodList() {
   return (
-  <div className='food-list'>
-<figure className='food-list__img'>
-    <img src="" alt="" />
-</figure>
-<div className='food-list__header'>
-<span className='food-list__header__title' >Karışık Izgara</span> 
-<span className='food-list__header__count' >1 Adet</span>
-</div>
-<div className='food-list__price' >180₺</div>
-
-  </div>
-  )
+    <div className='line-throught'>
+      <div className='food-list'>
+        {yemekler.map((yemek) => (
+          <div key={yemek.id}>
+            <div className='food-list__item'>
+              <figure className='food-list__item__img'>
+                <img src={yemek.resim} alt="food-img" />
+              </figure>
+              <div>
+                <div className='food-list__item__header'>
+                  <span className='food-list__item__header__title'>{yemek.isim}</span>
+                  <span className='food-list__item__header__count'>{yemek.adet}</span>
+                </div>
+              </div>
+              <div className='food-list__item__price'>{yemek.fiyat}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
